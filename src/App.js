e41530d12962aef8901c0c95be41143b83905b3c
Hamza AZZAOUI <azzaouihamza.g@gmail.com>
2022-04-03 04:14:07 +0000

    add live chat

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SigninPage from "./pages/signin";
+import ChatComponent from "./components/Chat";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" component={Home} exact />
-        <Route path="/signin" component={SigninPage} exact />
+        <Route path="/" element={<Home />} exact />
+        <Route path="/signin" element={<SigninPage ></SigninPage>} exact />
+        <Route path="/support" element={<ChatComponent ></ChatComponent>} exact />
       </Routes>
     </Router>
   );
---
+++ src/components/Chat/index.js
@@ -0,0 +1,47 @@
+import React,{ useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import 'stream-chat-react/dist/css/index.css';
+import Navbar from '../Navbar';
+import Sidebar from '../SideBar';
+
+
+const chatClient = StreamChat.getInstance('tg3kj42vtzru');
+
+chatClient.connectUser(
+  {
+    id: 'yassine',
+    name: 'yassine'
+  },
+  chatClient.devToken('yassine'),
+);
+const channel = chatClient.channel('messaging', 'MeetnFit', {
+    // add as many custom fields as you'd like
+
+    name: 'MeetnFit',
+    members: ['Hamza'],
+  });
+const ChatComponent = () =>{
+    const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+    return (
+    <div>
+        <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+<Chat client={chatClient} theme='messaging dark'>
+    <Channel channel={channel}>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+    </div>
+ 
+)};
+
+export default ChatComponent;
